7610615c3a56fd06f4976c507aa5bdb4a162a03b
lijunjing <1196036414@qq.com>
2020-10-07 20:15:28 +0800

    1

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,1 +1,32 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <h1>登录页面</h1>
+</head>
+<body>
+{{session('msg')}}
+<form action="{{url('user/logins')}}" method="post">
+    @csrf
+    <table>
+        <tr>
+            <td>用户名</td>
+            <td><input type="text" name="user_name"></td>
+        </tr>
+        <tr>
+            <td>密码</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td>添加</td>
+            <td><input type="submit" value="添加"></td>
+        </tr>
+
+    </table>
+</form>
+</body>
+</html>
